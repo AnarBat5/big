7b98a1fa55c3d4815233b514e67f76a56eb4b5ba
AnarBat5 <anarbatbayr34@gmail.com>
2026-05-04 19:19:56 +0800

    Add GitHub Pages static export config and Actions workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/big',
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
+    unoptimized: true,
   },
 };
 module.exports = nextConfig;
